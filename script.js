77e793a3189cd7db6dd86c0b0bc667e0f8aae9a8
TimvanAlphen <55843527+TimvanAlphen@users.noreply.github.com>
2020-10-22 13:15:24 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
 //console.log("Hello world!");
 
 const surveyAnswers = data
-console.log(surveyAnswers)
+//console.log(surveyAnswers)
 
-console.log(surveyAnswers[0])
+//console.log(surveyAnswers[0])
 
-console.log(surveyAnswers[3].hoeveelheidOomsEnTantes)
+//console.log(surveyAnswers[3].hoeveelheidOomsEnTantes)
 
-let belangrijksteOnderwerpen = []
+let kolomNaam = "schoenmaat"
+let lijstAntwoorden = getAnswersForQuestion(surveyAnswers, kolomNaam)
 
-for (answer in surveyAnswers){
-	console.log(surveyAnswers[answer].belangrijksteOnderwerpVanDezeTijd)
+console.log("Survey answers are:",lijstAntwoorden)
+
+
+
+
+
+
+function getAnswersForQuestion(answers, question){
+	let answersForQuestion = []
+  for (answer of answers){
+  	answersForQuestion.push(answer[question])
+ }
+	return answersForQuestion
 }
+
+
+
+
+//const specificAnswers = surveyAnswers.map(answer => answer[kolomNaam])
+
+//console.log(specificAnswers)
+
