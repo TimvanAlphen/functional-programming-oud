2bc7ac82f83e99b2b4026856796e2f298e8e5443
TimvanAlphen <55843527+TimvanAlphen@users.noreply.github.com>
2020-10-22 15:35:49 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
 //console.log("Hello world!");
 
+
+//The survey data is loaded from an .js file.
 const surveyAnswers = data
-//console.log(surveyAnswers)
 
-//console.log(surveyAnswers[0])
-
-//console.log(surveyAnswers[3].hoeveelheidOomsEnTantes)
-
-let kolomNaam = "schoenmaat"
+//Variables with the surveyAnwsers and collumnname of "schoenmaat"
+let kolomNaam = "vooropleiding"
 let lijstAntwoorden = getAnswersForQuestion(surveyAnswers, kolomNaam)
 
+//console.log van de lijst met antwoorden.
 console.log("Survey answers are:",lijstAntwoorden)
 
 
-
-
-
-
+//Function that saves an array with the answersForQuestions and pushes them to the variable "lijstAntwoorden".
 function getAnswersForQuestion(answers, question){
 	let answersForQuestion = []
   for (answer of answers){
@@ -26,9 +22,27 @@ function getAnswersForQuestion(answers, question){
 }
 
 
+//The filter function only returns 'MBO' Bron: https://www.youtube.com/watch?v=BMUiFMZr7vk&ab_channel=FunFunFunction
+let niveau = lijstAntwoorden.filter(function(lijstAntwoorden) {
+	return lijstAntwoorden === 'HAVO'
+})
+
+//Map adds an eomji "=)" after MBO.
+let emoji = niveau.map(function(niveau){
+	return niveau + ' =)'
+})
+
+console.log("Niveau is:" ,niveau)
+
+console.log(emoji)
 
 
-//const specificAnswers = surveyAnswers.map(answer => answer[kolomNaam])
+/*
+let dogs = [] 
+for (let i = 0; i < animals.length; i++) {
+	if (animals[i].species === 'dog')
+		dogs.push(animals[i])
+}
 
-//console.log(specificAnswers)
 
+*/
